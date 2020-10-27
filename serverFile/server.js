d6c96c2ad906b7cf79859349eb28fa6a473b2358
Matthew Hiatt <mhiatt62@gmail.com>
2020-10-27 15:52:20 -0500

    added comments for clarity and changed other words to declare more clarity overall

--- serverFile/server.js
+++ serverFile/server.js
@@ -1,4 +1,6 @@
-//Heroku server set up
+// Copy and pasted from previous examples when looking back at the unit homework.
+// Don't recall if the 'app.listen' goes before or after the 'app.use' portion.
+// **NOTE** if changes need to be made on this end I will do so after testing.
 
 const express = require("express");
 var PORT = process.env.PORT || 3001;
