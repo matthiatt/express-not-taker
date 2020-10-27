77890ca0abe773e7a43d2bdcb4beab52022cffa5
Matthew Hiatt <mhiatt62@gmail.com>
2020-10-27 16:12:24 -0500

    server.js additions - test

--- serverFile/server.js
+++ serverFile/server.js
@@ -1,10 +1,8 @@
 // Copy and pasted from previous examples when looking back at the unit homework.
 // Don't recall if the 'app.listen' goes before or after the 'app.use' portion.
 // **NOTE** if changes need to be made on this end I will do so after testing.
-
+var http = require('http');
 const express = require("express");
-var PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -14,7 +12,14 @@ app.use(express.static('public'));
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
+var PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server is listening port: ${PORT}`);
+function handleRequest(request, response) {
+  response.end("It Works!!" + request.url);
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
 });
