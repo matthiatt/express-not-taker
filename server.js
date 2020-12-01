1f88cd822cd3dfa5101e3836180f919064ca27b2
Matthew Hiatt <mhiatt62@gmail.com>
2020-12-01 14:43:57 -0600

    some edits after testing.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static("public"));
 
 require("./routes/apiRoute")(app);
 
